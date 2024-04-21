fbf8a738bd0f4bd6f2df19f0d52589be1982266f
Paul Dubovsky <pauldubovsky@gmail.com>
2024-04-21 15:11:09 -0700

    update models

--- backend/server/structs.go
+++ backend/server/structs.go
@@ -17,8 +17,13 @@ type Server struct {
 	DB     *gorm.DB
 }
 
-type Listing struct {
+type ListingEmbed struct {
 	gorm.Model
+	ListingID uuid.UUID `gorm:"primary_key"` // Unique identifier for a new listing
+}
+
+type Listing struct {
+	ListingEmbed
 	Category      string    `json:"category"`
 	Title         string    `json:"title"`                      // Title of the item being auctioned.
 	Description   string    `json:"description"`                // Detailed description of the item.
@@ -28,23 +33,31 @@ type Listing struct {
 	EndTime       time.Time `json:"end_time"`                   // Date and time when the auction ends.
 	Active        bool      `json:"active" gorm:"default:true"` // Boolean indicating whether the listing is currently active or inactive
 	UserID        uuid.UUID `json:"seller_id"`                  // User ID of the seller who created the listing.
-	User          User
+}
+
+type UserEmbed struct {
+	gorm.Model
+	UserID uuid.UUID `gorm:"primary_key"` // Unique identifier for each user
 }
 
 type User struct {
+	UserEmbed
+	FirstName    string    `json:"first_name"`                           // User first name
+	LastName     string    `json:"last_name"`                            // User last name
+	Username     string    `json:"username" gorm:"unique"`               // Username for login (should be unique).
+	Email        string    `json:"email" gorm:"unique"`                  // User's email address (should be unique).
+	PasswordHash string    `json:"password_hash"`                        // Hashed password for secure storage.
+	Listings     []Listing `json:"listings" gorm:"foreignKey:ListingID"` // the listings for this particular user
+}
+
+type BidEmbed struct {
 	gorm.Model
-	FirstName    string `json:"first_name"`             // User first name
-	LastName     string `json:"last_name"`              // User last name
-	Username     string `json:"username" gorm:"unique"` // Username for login (should be unique).
-	Email        string `json:"email" gorm:"unique"`    // User's email address (should be unique).
-	PasswordHash string `json:"password_hash"`          // Hashed password for secure storage.
+	BidID uuid.UUID `gorm:"primary_key"` // unique bid id
 }
 
 type Bid struct {
-	gorm.Model
+	BidEmbed
 	Amount    float64   `json:"amount"`     // Bid amount placed by the user.
 	ListingID uuid.UUID `json:"listing_id"` // ID of the auction listing the bid is placed on.
-	Listing   Listing
-	UserID    uuid.UUID `json:"bidder_id"` // User ID of the bidder who placed the bid.
-	User      User
+	UserID    uuid.UUID `json:"bidder_id"`  // User ID of the bidder who placed the bid.
 }
