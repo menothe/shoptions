a5828b850315053ba95b74c96760be3fdcd58184
Paul Dubovsky <pauldubovsky@gmail.com>
2024-05-22 17:16:21 -0700

    cleanup routing logic

--- backend/server/server.go
+++ backend/server/server.go
@@ -37,47 +37,13 @@ func (server *Server) SetupRoutes() {
 	apiGroup := server.Router.Group("/api")
 
 	//users
-	userGroup := apiGroup.Group("/users")
-	userGroup.Use(func(c *gin.Context) {
-		// Skip authentication for login and signup
-		if c.Request.URL.Path == "/api/users/login" || c.Request.URL.Path == "api/users/signup" {
-			c.Next()
-			return
-		}
-		server.UserHandler.RequireAuth(c)
-	})
-	userGroup.POST("/signup", server.UserHandler.SignUp)
-	userGroup.POST("/login", server.UserHandler.Login)
-	userGroup.POST("/logout", server.UserHandler.Logout)
+	server.initializeUserRoutes(apiGroup)
 
 	//listings
-	listingGroup := apiGroup.Group("/listings")
-	listingGroup.Use(func(c *gin.Context) {
-		//Skip authentication for getting all listings
-		if c.Request.URL.Path == "/api/listings/all" || c.Request.URL.Path == "/api/listings/by_query" {
-			c.Next()
-			return
-		}
-		server.UserHandler.RequireAuth(c)
-	})
-	listingGroup.POST("/create", server.ListingHandler.CreateListing)
-	listingGroup.PUT("/update/:id", server.ListingHandler.UpdateListing)
-	listingGroup.GET("/all", server.ListingHandler.GetAllListings)
-	listingGroup.GET("/by_user", server.ListingHandler.GetUsersListings)
-	listingGroup.POST("by_query", server.ListingHandler.SearchByQuery)
+	server.initializeListingRoutes(apiGroup)
 
 	//bids
-	bidGroup := apiGroup.Group("/bids")
-	bidGroup.Use(func(c *gin.Context) {
-		//Skip authentication for getting all listings
-		if c.Request.URL.Path == "/api/bids/highest_bidder" {
-			c.Next()
-			return
-		}
-		server.UserHandler.RequireAuth(c)
-	})
-	bidGroup.POST("/create", server.BidHandler.CreateBid)
-	bidGroup.GET("/highest_bidder/:id", server.BidHandler.GetHighestBidder)
+	server.initializeBidRoutes(apiGroup)
 
 	server.Router.Run("localhost:8080")
 }
@@ -99,4 +65,50 @@ func HandleMigrationError(err error, modelType any) {
 	if err != nil {
 		log.Fatalf("unable to run database migrations for table: %s", reflect.TypeOf(modelType))
 	}
+}
+
+func (server *Server) initializeUserRoutes(apiGroup *gin.RouterGroup) {
+	userGroup := apiGroup.Group("/users")
+	userGroup.Use(func(c *gin.Context) {
+		// Skip authentication for login and signup
+		if c.Request.URL.Path == "/api/users/login" || c.Request.URL.Path == "api/users/signup" {
+			c.Next()
+			return
+		}
+		server.UserHandler.RequireAuth(c)
+	})
+	userGroup.POST("/signup", server.UserHandler.SignUp)
+	userGroup.POST("/login", server.UserHandler.Login)
+	userGroup.POST("/logout", server.UserHandler.Logout)
+}
+
+func (server *Server) initializeListingRoutes(apiGroup *gin.RouterGroup) {
+	listingGroup := apiGroup.Group("/listings")
+	listingGroup.Use(func(c *gin.Context) {
+		//Skip authentication for getting all listings
+		if c.Request.URL.Path == "/api/listings/all" || c.Request.URL.Path == "/api/listings/by_query" {
+			c.Next()
+			return
+		}
+		server.UserHandler.RequireAuth(c)
+	})
+	listingGroup.POST("/create", server.ListingHandler.CreateListing)
+	listingGroup.PUT("/update/:id", server.ListingHandler.UpdateListing)
+	listingGroup.GET("/all", server.ListingHandler.GetAllListings)
+	listingGroup.GET("/by_user", server.ListingHandler.GetUsersListings)
+	listingGroup.POST("by_query", server.ListingHandler.SearchByQuery)
+}
+
+func (server *Server) initializeBidRoutes(apiGroup *gin.RouterGroup) {
+	bidGroup := apiGroup.Group("/bids")
+	bidGroup.Use(func(c *gin.Context) {
+		//Skip authentication for getting all listings
+		if c.Request.URL.Path == "/api/bids/highest_bidder" {
+			c.Next()
+			return
+		}
+		server.UserHandler.RequireAuth(c)
+	})
+	bidGroup.POST("/create", server.BidHandler.CreateBid)
+	bidGroup.GET("/highest_bidder/:id", server.BidHandler.GetHighestBidder)
 }
